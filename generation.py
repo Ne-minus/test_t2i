90927b6d418d8a6794129c9d1356d3d7bc00f930
Ne-minus <72148999+Ne-minus@users.noreply.github.com>
2024-08-06 09:25:38 +0300

    Update generation.py

--- generation.py
+++ generation.py
@@ -1,38 +1,44 @@
 import torch
 import huggingface_hub
-from dataset_smapler import GenerationDataset
+from dataset_sampler import GenerationDataset
 from diffusers import StableDiffusionPipeline, AutoPipelineForText2Image, Transformer2DModel, PixArtSigmaPipeline, StableDiffusion3Pipeline, DiffusionPipeline
 from PIL import Image
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 import os
 import yaml
 
 with open(r"./t2i_configs.yml") as file:
     params_list = yaml.load(file, Loader=yaml.FullLoader)
 
-# huggingface_hub.login(token='YOUR HF ACCESS TOKEN')
-    
+
+huggingface_hub.login(token='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')
+
 class InitializeModels:
     def __init__(self, model_name, outputdir_name):
         self.model_path = model_name
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
- 
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if torch.cuda.is_available():
+            print(f"Using GPU: {torch.cuda.current_device()}")
+            print(f"Device name: {torch.cuda.get_device_name(torch.cuda.current_device())}")
+        else:
+            print("CUDA is not available")
         dir_name = model_name.replace('/', '_')
 
         self.dir  = f"{outputdir_name}/{dir_name}"
         if not os.path.exists(self.dir):
             os.mkdir(self.dir)
-  
+
         self.pipe_def()
-
-
-    def pipe_def(self):
+        
+def pipe_def(self):
 
         if self.model_path == 'stabilityai/stable-diffusion-3-medium-diffusers':
-            self.pipe = StableDiffusion3Pipeline.from_pretrained(self.model_path, 
-                                                                 torch_dtype=torch.float16, 
+            self.pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3-medium-diffusers", torch_dtype=torch.float16)
+            self.pipe = StableDiffusion3Pipeline.from_pretrained(self.model_path,
+                                                                 torch_dtype=torch.float16,
                                                                  variant="fp16")
-        
+
         elif self.model_path == 'stabilityai/stable-diffusion-xl-base-1.0' or  self.model_path == 'playgroundai/playground-v2.5-1024px-aesthetic':
             self.pipe = DiffusionPipeline.from_pretrained(self.model_path, torch_dtype=torch.float16)
 
@@ -44,11 +50,11 @@ class InitializeModels:
 
         elif self.model_path == 'PixArt-alpha/PixArt-Sigma-XL-2-512-MS':
             self.pipe = PixArtSigmaPipeline.from_pretrained(
-                        "PixArt-alpha/PixArt-Sigma-XL-2-1024-MS", 
+                        "PixArt-alpha/PixArt-Sigma-XL-2-1024-MS",
                         torch_dtype=torch.float16,
                         use_safetensors=True,
                         )
-            
+
         elif self.model_path == "DeepFloyd/IF-I-XL-v1.0":
             self.pipe = DiffusionPipeline.from_pretrained("DeepFloyd/IF-I-XL-v1.0", variant="fp16", torch_dtype=torch.float16)
             self.pipe.enable_model_cpu_offload()
@@ -57,13 +63,11 @@ class InitializeModels:
 
 
     def generate_image(self, batch):
-        batch_lemmas = [i[1] for i in batch]
-        batch_ids = [i[0] for i in batch]
-        images = self.pipe(batch_lemmas, 
+        images = self.pipe(list(batch[1]),
                             num_inference_steps=28,
                             guidance_scale=7.0,).images
-        
-        for idx, pic in zip(batch_ids, images):
+
+        for idx, pic in zip(batch[0].tolist(), images):
             pic.save(f"{self.dir}/img_{idx}.png")
 
 
@@ -75,5 +79,5 @@ if __name__ == '__main__':
 
     model = InitializeModels(params_list["MODEL_NAME"][0], params_list["OUTPUT_DIR"][0])
 
-    for batch in loader:
-        model.generate_image(batch)
+    for batch in tqdm(loader):
+        model.generate_imagebatch)
